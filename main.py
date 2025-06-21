4e6c0a88feb858f4d0ad0a0bb410f9d1e7533372
Laurian1906 <laur.laury@gmail.com>
2025-06-21 12:11:56 +0300

    Update: Added logs directory in .gitignore, this will be used to only see the logs of main.py. Main.py was updated in using logging.basicConfig

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ from utils.env_loader import GEMINI_API_KEY
 
 client = genai.Client(api_key=GEMINI_API_KEY)
 
+# Set up the logs for the file
+logging.basicConfig(
+    filename='logs/main.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
 try:
     response = client.models.generate_content(
         model="gemini-2.0-flash",
@@ -22,5 +30,5 @@ try:
     )
     print(response.text)
 except Exception as e:
-    logging.error("An error occured while generating the content: " + str(e))
+    logging.exception("An error occured while generating the content: " + str(e))
 
