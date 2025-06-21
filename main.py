023cc89984f5d03512158c932ac0e4e579feb64a
Laurian1906 <laur.laury@gmail.com>
2025-06-21 11:58:13 +0300

    Update: Added try-except around the client.models.generate_content and resolved a typo in songs_data.json

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """
 The main part of the code which runs the AI
 """
+import logging
 
 # IMPORTS
 from google import genai
@@ -12,11 +13,14 @@ from utils.env_loader import GEMINI_API_KEY
 
 client = genai.Client(api_key=GEMINI_API_KEY)
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    config=types.GenerateContentConfig(
-        system_instruction=PROMPT),
-    contents=USER_PROMPT
-)
+try:
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        config=types.GenerateContentConfig(
+            system_instruction=PROMPT),
+        contents=USER_PROMPT
+    )
+    print(response.text)
+except Exception as e:
+    logging.error("An error occured while generating the content: " + str(e))
 
-print(response.text)
